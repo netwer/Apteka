9ce15e6e707738127e294de80c985dfbb131f900
art_net <art9207@gmail.com>
2015-03-21 09:22:35 +0300

    added empty proj

---
+++ src/main/java/org/apteka/AptekaApplication.java
@@ -0,0 +1,12 @@
+package org.apteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptekaApplication.class, args);
+    }
+}
