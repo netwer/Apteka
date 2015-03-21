ee4845becda74dece1f8936d40f8b1a3cb13170d
art_net <art9207@gmail.com>
2015-03-21 10:00:59 +0300

    empty proj

---
+++ src/main/java/org/apteka/ApiController.java
@@ -0,0 +1,18 @@
+package org.apteka;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Admin on 21.03.2015.
+ */
+@Controller
+@RequestMapping("/Api")
+public class ApiController {
+    @RequestMapping("/")
+    public @ResponseBody
+    String sayHello(){
+        return "Hi!!!, this is Spring Bo22222222ot Project for IS 'Apteka'";
+    }
+}
