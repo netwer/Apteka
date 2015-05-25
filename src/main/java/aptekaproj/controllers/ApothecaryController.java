1f994145eb4a04c3430184580489b10f51bbc105
art_net <art9207@gmail.com>
2015-05-25 09:24:48 +0300

    + Patient, Pharmacies, Apothecary empty controllers

---
+++ src/main/java/aptekaproj/controllers/ApothecaryController.java
@@ -0,0 +1,7 @@
+package aptekaproj.controllers;
+
+/**
+ * Created by Admin on 25.05.2015.
+ */
+public class ApothecaryController {
+}
---
+++ src/main/java/aptekaproj/controllers/PharmaciesController.java
@@ -0,0 +1,14 @@
+package aptekaproj.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Admin on 25.05.2015.
+ */
+
+//Провизор
+@Controller
+@RequestMapping("/Pharmacies")
+public class PharmaciesController {
+}
