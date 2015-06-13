7e8203f8574c8601520e2dfb98f3d881a1b79070
art_net <art9207@gmail.com>
2015-06-13 12:37:16 +0300

    * DateWorker * TimeIgnoringComparator

--- src/main/java/aptekaproj/helpers/DateWorker.java
+++ src/main/java/aptekaproj/helpers/DateWorker.java
@@ -1,7 +1,25 @@
 package aptekaproj.helpers;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by Admin on 13.06.2015.
  */
 public class DateWorker {
+
+    public static Date AddDaysToDate(Date currentDate,int countDays){
+        Date date = currentDate;
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, countDays);
+        return c.getTime();
+    }
+
+    public static Date MaxDate(Date d1,Date d2){
+        if(TimeIgnoringComparator.compare(d1,d2) >= 0)
+            return d1;
+        else
+            return d2;
+    }
 }
--- src/main/java/aptekaproj/helpers/TimeIgnoringComparator.java
+++ src/main/java/aptekaproj/helpers/TimeIgnoringComparator.java
@@ -1,7 +1,28 @@
 package aptekaproj.helpers;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by Admin on 13.06.2015.
  */
-public class TimeIgnoringComparator {
+public class TimeIgnoringComparator implements Comparable<Date> {
+    @Override
+    public int compareTo(Date o) {
+        return 0;
+    }
+
+    public static int compare(Date d1, Date d2) {
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(d1);
+
+        Calendar c2 = Calendar.getInstance();
+        c2.setTime(d2);
+
+        if (c1.get(Calendar.YEAR) != c2.get(Calendar.YEAR))
+            return c1.get(Calendar.YEAR) - c2.get(Calendar.YEAR);
+        if (c1.get(Calendar.MONTH) != c2.get(Calendar.MONTH))
+            return c1.get(Calendar.MONTH) - c2.get(Calendar.MONTH);
+        return c1.get(Calendar.DAY_OF_MONTH) - c2.get(Calendar.DAY_OF_MONTH);
+    }
 }
