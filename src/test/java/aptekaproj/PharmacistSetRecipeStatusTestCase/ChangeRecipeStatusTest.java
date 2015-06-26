8f1d17ffb7905560c6770b62503b5f3969e43271
art_net <art9207@gmail.com>
2015-06-27 00:51:04 +0300

    + tests for test-case "set status in recipe to done by pharmacist"

---
+++ src/test/java/aptekaproj/PharmacistSetRecipeStatusTestCase/ChangeRecipeStatusTest.java
@@ -0,0 +1,7 @@
+package aptekaproj.PharmacistSetRecipeStatusTestCase;
+
+/**
+ * Created by org.apteka on 27.06.2015.
+ */
+public class ChangeRecipeStatusTest {
+}
---
+++ src/test/java/aptekaproj/PharmacistSetRecipeStatusTestCase/GetRecipeTest.java
@@ -0,0 +1,40 @@
+package aptekaproj.PharmacistSetRecipeStatusTestCase;
+
+import aptekaproj.AptekaApplication;
+import aptekaproj.services.RecipeService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by org.apteka on 27.06.2015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AptekaApplication.class)
+public class GetRecipeTest {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    private int recipeId;
+    private String status;
+    private boolean result;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void changeRecipeStatus() throws Exception {
+    }
+}
